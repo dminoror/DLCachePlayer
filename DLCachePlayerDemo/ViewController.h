d85445a3de277170b7bd6fc56b533fbd1d81118b
DoubleLight <dohhlight@gmail.com>
2018-07-19 14:43:47 +0800

    Demo 大致完工

--- DLCachePlayerDemo/ViewController.h
+++ DLCachePlayerDemo/ViewController.h
@@ -8,11 +8,22 @@
 
 #import <UIKit/UIKit.h>
 #import "DLCachePlayer.h"
+#import "DLProgressSlider.h"
 
 @interface ViewController : UIViewController<DLCachePlayerDataDelegate, DLCachePlayerStateDelegate, UITableViewDelegate, UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UITableView *musicTable;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *btnPlayPause;
 - (IBAction)btnPlayPause_Clicked:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *btnNext;
+- (IBAction)btnNext_Clicked:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *btnPrev;
+- (IBAction)btnPrev_Clicked:(id)sender;
+
+@property (weak, nonatomic) IBOutlet DLProgressSlider *progressBar;
+- (IBAction)progressBar_Changed:(id)sender;
 
 @end
 
